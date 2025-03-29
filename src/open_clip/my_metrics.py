38670ae1d5c2ad555f26e7708ccb2e20de903426
nickxir12 <nikolasxiros2002@gmail.com>
2025-03-29 20:07:07 +0200

    adding metrics

--- src/open_clip/my_metrics.py
+++ src/open_clip/my_metrics.py
@@ -181,3 +181,129 @@ def evaluate_model(
     df_results = pd.DataFrame(results)
 
     return df_results
+
+
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+#                               FROM CYCLIP
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+
+
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx : min(ndx + n, l)]
+
+
+@torch.no_grad()
+def itm_eval(text_embeddings, image_embeddings):
+
+    # sim_matrix_i2t = image_embeddings @ text_embeddings.t()
+    # sim_matrix_t2i = text_embeddings @ image_embeddings.t()
+
+    ## Image -> Text
+    # ranks = np.zeros(len(sim_matrix_i2t))
+    ranks = np.zeros(len(image_embeddings))
+
+    for index in range(0, len(image_embeddings), 5):
+        scores = image_embeddings[index] @ text_embeddings.t()
+        # scores = sim_matrix_i2t[index]
+        li = np.argsort(scores.detach().cpu().numpy())[::-1]
+        for i in range(len(li)):
+            if index <= li[i] and li[i] <= index + 4:
+                rank = i
+                break
+        ranks[index] = rank
+
+        # Compute metrics
+    tr1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    tr5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    tr10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+
+    ## Image -> Text
+    ranks = np.zeros(len(text_embeddings))
+    for index in range(len(text_embeddings)):
+        scores = text_embeddings[index] @ image_embeddings.t()
+        # for index, scores in tqdm(enumerate(sim_matrix_t2i)):
+        scores = scores[::5]
+        li = np.argsort(scores.detach().cpu().numpy())[::-1]
+        for i in range(len(li)):
+            if li[i] == index // 5:
+                rank = i
+                break
+        ranks[index] = rank
+
+    # Compute metrics
+    ir1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    ir5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    ir10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+
+    tr_mean = (tr1 + tr5 + tr10) / 3
+    ir_mean = (ir1 + ir5 + ir10) / 3
+    r_mean = (tr_mean + ir_mean) / 2
+
+    eval_result = {
+        "txt_r1": tr1,
+        "txt_r5": tr5,
+        "txt_r10": tr10,
+        "txt_r_mean": tr_mean,
+        "img_r1": ir1,
+        "img_r5": ir5,
+        "img_r10": ir10,
+        "img_r_mean": ir_mean,
+        "r_mean": r_mean,
+    }
+
+    return eval_result
+
+
+def get_all_embeddings(
+    model,
+    all_texts,
+    all_images,
+    root,
+    preprocess,
+    tokenizer,
+    batch_size=1024,
+    device=None,
+):
+    if device is None:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    text_embeddings = []
+    image_embeddings = []
+
+    with torch.no_grad():
+        dataloader_texts = list(batch(all_texts, batch_size))
+        dataloader_images = list(batch(all_images, batch_size))
+
+        bar = zip(dataloader_texts, dataloader_images)
+        bar = tqdm(bar, total=len(dataloader_texts), desc="Encoding batches")
+
+        for texts, images in bar:
+            # Tokenize text
+            text_tokens = tokenizer(texts).to(device)
+
+            # Preprocess and stack images
+            image_tensors = torch.stack(
+                [
+                    preprocess(Image.open(os.path.join(root, img)).convert("RGB"))
+                    for img in images
+                ]
+            ).to(device)
+
+            # Encode
+            image_embedding = model.encode_image(image_tensors)
+            text_embedding = model.encode_text(text_tokens)
+
+            # Normalize
+            text_embedding /= text_embedding.norm(dim=-1, keepdim=True)
+            image_embedding /= image_embedding.norm(dim=-1, keepdim=True)
+
+            text_embeddings.append(text_embedding)
+            image_embeddings.append(image_embedding)
+
+        text_embeddings = torch.cat(text_embeddings)
+        image_embeddings = torch.cat(image_embeddings)
+        return text_embeddings, image_embeddings
